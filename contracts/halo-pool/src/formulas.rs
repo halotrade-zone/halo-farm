840975e0a53b5d295e83fc7c343635e7afd250ec
HaiTranQuang <haitranwang@gmail.com>
2023-05-30 16:38:17 +0700

    remove redundant function

--- contracts/halo-pool/src/formulas.rs
+++ contracts/halo-pool/src/formulas.rs
@@ -1,21 +1,3 @@
-use crate::state::PoolInfo;
-use cosmwasm_std::Uint128;
-
-pub fn calc_reward(pool_info: &PoolInfo, current_time: u64) -> Uint128 {
-    let _reward_per_second = pool_info.reward_per_second;
-    let start_time = pool_info.start_time;
-    let end_time = pool_info.end_time;
-
-    if current_time < start_time {
-        return Uint128::zero();
-    }
-
-    if current_time >= end_time {
-        return (end_time - start_time).into();
-    }
-    (current_time - start_time).into()
-}
-
 /// Returns the multiplier over the given _from_ and _to_ range.
 /// The multiplier is zero if the _to_ range is before the _end_.
 /// The multiplier is the _end_ minus _from_ if the _from_ range is after the _end_.
