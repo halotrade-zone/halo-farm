e752a7587a016211ecc7f53a233fa6f6eb3686f1
HaiTranQuang <haitranwang@gmail.com>
2023-08-14 14:17:19 +0700

    remove redundant elements in ContractError enum

--- contracts/halo-farm/src/error.rs
+++ contracts/halo-farm/src/error.rs
@@ -1,29 +1,8 @@
-use cosmwasm_std::{OverflowError, StdError};
+use cosmwasm_std::StdError;
 use thiserror::Error;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
-
-    #[error("{0}")]
-    OverflowError(#[from] OverflowError),
-
-    #[error("Unauthorized")]
-    Unauthorized {},
-
-    #[error("Invalid zero amount")]
-    InvalidZeroAmount {},
-
-    #[error("Max spread assertion")]
-    MaxSpreadAssertion {},
-
-    #[error("Max slippage assertion")]
-    MaxSlippageAssertion {},
-
-    #[error("Asset mismatch")]
-    AssetMismatch {},
-
-    #[error("Insufficient Funds")]
-    InsufficientFunds {},
 }
